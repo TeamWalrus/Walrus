1f8a200cafbcd46bb446231e4db8114e21a62072
Matthew Daley <mattd@bugfuzz.com>
2017-05-01 20:46:59 +1200

    Add result intents, add card data class

---
+++ app/src/main/java/com/bugfuzz/android/projectwalrus/CardData.java
@@ -0,0 +1,14 @@
+package com.bugfuzz.android.projectwalrus;
+
+import org.parceler.Parcel;
+
+@Parcel
+public final class CardData {
+    public enum Type {
+        HID,
+        MIFARE
+    };
+
+    public Type type;
+    public String data;
+}
--- app/src/main/java/com/bugfuzz/android/projectwalrus/CardDeviceIntentService.java
+++ app/src/main/java/com/bugfuzz/android/projectwalrus/CardDeviceIntentService.java
@@ -3,11 +3,17 @@ package com.bugfuzz.android.projectwalrus;
 import android.app.IntentService;
 import android.content.Intent;
 import android.content.Context;
+import android.support.v4.content.LocalBroadcastManager;
+
+import org.parceler.Parcels;
 
 public class CardDeviceIntentService extends IntentService {
     private static final String ACTION_READ_CARD_DATA = "com.bugfuzz.android.projectwalrus.action.READ_CARD_DATA";
+    private static final String ACTION_READ_CARD_DATA_RESULT = "com.bugfuzz.android.projectwalrus.action.READ_CARD_DATA_RESULT";
     private static final String ACTION_WRITE_CARD_DATA = "com.bugfuzz.android.projectwalrus.action.WRITE_CARD_DATA";
+    private static final String ACTION_WRITE_CARD_DATA_RESULT = "com.bugfuzz.android.projectwalrus.action.WRITE_CARD_DATA_RESULT";
 
+    private static final String EXTRA_OPERATION_ID = "com.bugfuzz.android.projectwalrus.extra.OPERATION_ID";
     private static final String EXTRA_CARD_DATA = "com.bugfuzz.android.projectwalrus.extra.CARD_DATA";
 
     public CardDeviceIntentService() {
@@ -29,21 +35,30 @@ public class CardDeviceIntentService extends IntentService {
 
     @Override
     protected void onHandleIntent(Intent intent) {
-        if (intent != null) {
-            final String action = intent.getAction();
-            if (ACTION_READ_CARD_DATA.equals(action))
-                handleActionReadCardData();
-            else if (ACTION_WRITE_CARD_DATA.equals(action))
-                handleActionWriteCardData(intent.getStringExtra(EXTRA_CARD_DATA));
-        }
+        if (intent == null)
+            return;
+
+        final String action = intent.getAction();
+        Intent result = new Intent(this, CardDeviceIntentService.class);
+
+        if (ACTION_READ_CARD_DATA.equals(action))
+            handleActionReadCardData(result);
+        else if (ACTION_WRITE_CARD_DATA.equals(action))
+            handleActionWriteCardData(result,
+                    (CardData)Parcels.unwrap(intent.getParcelableExtra(EXTRA_CARD_DATA)));
+        else
+            return;
+
+        result.putExtra(EXTRA_OPERATION_ID, intent.getParcelableExtra(EXTRA_OPERATION_ID));
+        LocalBroadcastManager.getInstance(this).sendBroadcast(result);
     }
 
-    private void handleActionReadCardData() {
+    private void handleActionReadCardData(Intent result) {
         // TODO: Handle action
         throw new UnsupportedOperationException("Not yet implemented");
     }
 
-    private void handleActionWriteCardData(String cardData) {
+    private void handleActionWriteCardData(Intent result, CardData cardData) {
         // TODO: Handle action
         throw new UnsupportedOperationException("Not yet implemented");
     }
