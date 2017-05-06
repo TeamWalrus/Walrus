d4ebdb02ceca28873f0b51d48561f93da798d3e7
Matthew Daley <mattd@bugfuzz.com>
2017-05-07 01:24:04 +1200

    Chameleon Mini card device class

---
+++ app/src/main/java/com/bugfuzz/android/projectwalrus/carddevice/ChameleonMiniDevice.java
@@ -0,0 +1,18 @@
+package com.bugfuzz.android.projectwalrus.carddevice;
+
+import com.bugfuzz.android.projectwalrus.CardData;
+
+@CardDevice.UsbDevice(vendorId = 5840, productId = 1202)
+public class ChameleonMiniDevice implements CardDevice {
+    public ChameleonMiniDevice(UsbDevice usbDevice) {
+
+    }
+
+    public CardData readCardData() {
+        throw new UnsupportedOperationException("Not yet implemented");
+    }
+
+    public void writeCardData(CardData cardData) {
+        throw new UnsupportedOperationException("Not yet implemented");
+    }
+}
