b9a72fe06ac87ce48de9eedbe57f497fdcfce4bd
Daniel Underhay <daniel.underhay@outlook.com>
2017-06-10 19:33:09 +1200

    start on location service

--- app/src/main/java/com/bugfuzz/android/projectwalrus/ui/AddCardActivity.java
+++ app/src/main/java/com/bugfuzz/android/projectwalrus/ui/AddCardActivity.java
@@ -1,12 +1,25 @@
 package com.bugfuzz.android.projectwalrus.ui;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.bugfuzz.android.projectwalrus.R;
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.tasks.OnSuccessListener;
 
 public class AddCardActivity extends AppCompatActivity {
+
+    TextView cardLocation;
+    private FusedLocationProviderClient mFusedLocationClient;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -14,5 +27,33 @@ public class AddCardActivity extends AppCompatActivity {
         setContentView(R.layout.activity_add_card);
         Toolbar myToolbar = (Toolbar) findViewById(R.id.terminal_toolbar);
         setSupportActionBar(myToolbar);
+
+
+        cardLocation = (TextView)findViewById(R.id.textView_CardLocation);
+        mFusedLocationClient = LocationServices.getFusedLocationProviderClient(this);
+
+
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return;
+        }
+        mFusedLocationClient.getLastLocation()
+                .addOnSuccessListener(this, new OnSuccessListener<Location>() {
+                    @Override
+                    public void onSuccess(Location location) {
+                        // Got last known location. In some rare situations this can be null.
+                        if (location != null) {
+                            cardLocation.setText(location.getLatitude() + ", " + location.getLongitude());
+                        }
+                    }
+                });
+
     }
+
 }
