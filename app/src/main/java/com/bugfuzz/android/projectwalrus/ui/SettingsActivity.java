17f857edea7a8d0c0750f73feee4b7b723482ffc
Matthew Daley <mattd@bugfuzz.com>
2017-08-19 23:45:57 +1200

    Add device settings to global settings

--- app/src/main/java/com/bugfuzz/android/projectwalrus/ui/SettingsActivity.java
+++ app/src/main/java/com/bugfuzz/android/projectwalrus/ui/SettingsActivity.java
@@ -1,5 +1,8 @@
 package com.bugfuzz.android.projectwalrus.ui;
 
+import android.content.Intent;
+import android.preference.Preference;
+import android.preference.PreferenceCategory;
 import android.preference.PreferenceFragment;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
@@ -7,6 +10,8 @@ import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 
 import com.bugfuzz.android.projectwalrus.R;
+import com.bugfuzz.android.projectwalrus.device.CardDevice;
+import com.bugfuzz.android.projectwalrus.device.CardDeviceManager;
 
 public class SettingsActivity extends AppCompatActivity {
 
@@ -18,6 +23,20 @@ public class SettingsActivity extends AppCompatActivity {
             // Load the preferences from an XML resource
             addPreferencesFromResource(R.xml.preferences);
 
+            PreferenceCategory devicePrefs = new PreferenceCategory(getContext());
+            devicePrefs.setTitle("Device Settings");
+            getPreferenceScreen().addPreference(devicePrefs);
+            
+            for (CardDevice cardDevice : CardDeviceManager.INSTANCE.getCardDevices().values()) {
+                Intent intent = cardDevice.getDeviceActivityIntent(getContext());
+                if (intent != null) {
+                    Preference preference = new Preference(getContext());
+                    preference.setTitle(cardDevice.getClass().getAnnotation(
+                            CardDevice.Metadata.class).name());
+                    preference.setIntent(intent);
+                    devicePrefs.addPreference(preference);
+                }
+            }
         }
     }
 
@@ -29,6 +48,5 @@ public class SettingsActivity extends AppCompatActivity {
         getFragmentManager().beginTransaction()
                 .replace(android.R.id.content, new SettingsFragment())
                 .commit();
-
     }
 }
