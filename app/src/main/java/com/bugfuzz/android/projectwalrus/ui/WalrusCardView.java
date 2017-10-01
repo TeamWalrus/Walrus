6aa9723d2158c47805d06908c795556f20fd5a91
Matthew Daley <mattd@bugfuzz.com>
2017-10-01 17:53:25 +1300

    Ensure WalrusCardView card object stays in sync with UI

--- app/src/main/java/com/bugfuzz/android/projectwalrus/ui/WalrusCardView.java
+++ app/src/main/java/com/bugfuzz/android/projectwalrus/ui/WalrusCardView.java
@@ -2,6 +2,8 @@ package com.bugfuzz.android.projectwalrus.ui;
 
 import android.content.Context;
 import android.content.res.TypedArray;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.util.AttributeSet;
 import android.view.View;
 import android.widget.EditText;
@@ -46,6 +48,23 @@ public class WalrusCardView extends FrameLayout {
         editableNameView = (EditText) view.findViewById(R.id.editableName);
         logoView = (ImageView) view.findViewById(R.id.logo);
         humanReadableTextView = (TextView) view.findViewById(R.id.humanReadableText);
+
+        editableNameView.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                card.name = editableNameView.getText().toString();
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+            }
+        });
 
         TypedArray a = getContext().obtainStyledAttributes(attrs, R.styleable.WalrusCardView, defStyle, 0);
 
