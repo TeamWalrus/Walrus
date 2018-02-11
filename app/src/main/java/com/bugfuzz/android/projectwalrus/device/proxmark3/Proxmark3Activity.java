329ce5c716a71896315d4763f86c509279e296ec
Matthew Daley <mattd@bugfuzz.com>
2018-02-11 22:57:25 +1300

    Show tune results in an AlertDialog instead of Proxmark3Activity

--- app/src/main/java/com/bugfuzz/android/projectwalrus/device/proxmark3/Proxmark3Activity.java
+++ app/src/main/java/com/bugfuzz/android/projectwalrus/device/proxmark3/Proxmark3Activity.java
@@ -1,12 +1,14 @@
 package com.bugfuzz.android.projectwalrus.device.proxmark3;
 
+import android.app.AlertDialog;
 import android.app.ProgressDialog;
-import android.graphics.Color;
+import android.content.DialogInterface;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Pair;
 import android.view.View;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -16,8 +18,10 @@ import com.bugfuzz.android.projectwalrus.device.CardDevice;
 import com.bugfuzz.android.projectwalrus.device.CardDeviceManager;
 
 import java.io.IOException;
+import java.lang.ref.WeakReference;
 
 public class Proxmark3Activity extends AppCompatActivity {
+
     public static final String EXTRA_DEVICE = "com.bugfuzz.android.projectwalrus.device.proxmark3.Proxmark3Activity.EXTRA_DEVICE";
 
     private Proxmark3Device proxmark3Device;
@@ -27,12 +31,6 @@ public class Proxmark3Activity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
 
         setContentView(R.layout.activity_proxmark3);
-
-        setSupportActionBar((Toolbar) findViewById(R.id.toolbar));
-
-        ActionBar actionBar = getSupportActionBar();
-        if (actionBar != null)
-            actionBar.setDisplayHomeAsUpEnabled(true);
 
         CardDevice cardDevice = CardDeviceManager.INSTANCE.getCardDevices().get(
                 getIntent().getIntExtra(EXTRA_DEVICE, -1));
@@ -48,6 +46,9 @@ public class Proxmark3Activity extends AppCompatActivity {
         }
 
         setSupportActionBar((Toolbar) findViewById(R.id.toolbar));
+        ActionBar actionBar = getSupportActionBar();
+        if (actionBar != null)
+            actionBar.setDisplayHomeAsUpEnabled(true);
 
         ((TextView) findViewById(R.id.version)).setText("Retrieving...");
 
@@ -66,49 +67,6 @@ public class Proxmark3Activity extends AppCompatActivity {
                 ((TextView) findViewById(R.id.version)).setText(version != null ? version : "(Unable to determine)");
             }
         }).execute();
-
-        updateTuneResult(null);
-    }
-
-    private void updateTuneResult(Proxmark3Device.TuneResult tuneResult) {
-        ((TextView) findViewById(R.id.lf_125)).setText(
-                tuneResult != null ? "" + tuneResult.v_125 + "V" : "");
-        ((TextView) findViewById(R.id.lf_134)).setText(
-                tuneResult != null ? "" + tuneResult.v_134 + "V" : "");
-        ((TextView) findViewById(R.id.lf_optimal)).setText(
-                tuneResult != null ? "" + tuneResult.peak_v + "V / " +
-                        (tuneResult.peak_f / 1000) + "kHz" : "");
-        ((TextView) findViewById(R.id.hf)).setText(
-                tuneResult != null ? "" + tuneResult.v_HF + "V" : "");
-
-        String lf_ok = "OK";
-        int lf_ok_color = Color.rgb(0, 0x80, 0);
-        if (tuneResult == null || tuneResult.peak_v < 2.948) {
-            lf_ok = "Unusable";
-            lf_ok_color = Color.rgb(0xff, 0, 0);
-        } else if (tuneResult.peak_v < 14.730) {
-            lf_ok = "Marginal";
-            lf_ok_color = Color.rgb(0x80, 0x80, 0);
-        }
-        ((TextView) findViewById(R.id.lf_ok)).setText(lf_ok);
-        ((TextView) findViewById(R.id.lf_ok)).setTextColor(lf_ok_color);
-
-        /* TODO: don't duplicate code */
-        String hf_ok = "OK";
-        int hf_ok_color = Color.rgb(0, 0x80, 0);
-        if (tuneResult == null || tuneResult.v_HF < 3.167) {
-            hf_ok = "Unusable";
-            hf_ok_color = Color.rgb(0xff, 0, 0);
-        } else if (tuneResult.v_HF < 7.917) {
-            hf_ok = "Marginal";
-            hf_ok_color = Color.rgb(0x80, 0x80, 0);
-        }
-        ((TextView) findViewById(R.id.hf_ok)).setText(hf_ok);
-        ((TextView) findViewById(R.id.hf_ok)).setTextColor(hf_ok_color);
-    }
-
-    private void tune(boolean lf) {
-        (new Proxmark3Activity.TuneTask(this, lf)).execute();
     }
 
     public void onTuneLFClick(View view) {
@@ -119,13 +77,21 @@ public class Proxmark3Activity extends AppCompatActivity {
         tune(false);
     }
 
-    private class TuneTask extends AsyncTask<Void, Void, Proxmark3Device.TuneResult> {
-        private Proxmark3Activity activity;
-        private boolean lf;
-        private ProgressDialog progressDialog = new ProgressDialog(Proxmark3Activity.this);
+    private void tune(boolean lf) {
+        new TuneTask(this, lf).execute();
+    }
 
-        public TuneTask(Proxmark3Activity activity, boolean lf) {
-            this.activity = activity;
+    private static class TuneTask extends AsyncTask<Void, Void,
+            Pair<Proxmark3Device.TuneResult, IOException>> {
+
+        private WeakReference<Proxmark3Activity> activity;
+
+        private boolean lf;
+
+        private ProgressDialog progressDialog;
+
+        TuneTask(Proxmark3Activity activity, boolean lf) {
+            this.activity = new WeakReference<>(activity);
             this.lf = lf;
         }
 
@@ -133,28 +99,64 @@ public class Proxmark3Activity extends AppCompatActivity {
         protected void onPreExecute() {
             super.onPreExecute();
 
+            Proxmark3Activity proxmark3Activity = activity.get();
+            if (proxmark3Activity == null) {
+                cancel(false);
+                return;
+            }
+
+            progressDialog = new ProgressDialog(proxmark3Activity);
             progressDialog.setMessage("Tuning...");
+            progressDialog.setCancelable(false);
             progressDialog.show();
         }
 
         @Override
-        protected Proxmark3Device.TuneResult doInBackground(Void... params) {
-            try {
-                return proxmark3Device.tune(lf, !lf);
-            } catch (IOException e) {
+        protected Pair<Proxmark3Device.TuneResult, IOException> doInBackground(Void... params) {
+            Proxmark3Activity proxmark3Activity = activity.get();
+            if (proxmark3Activity == null)
                 return null;
+
+            try {
+                return new Pair<>(proxmark3Activity.proxmark3Device.tune(lf, !lf), null);
+            } catch (IOException exception) {
+                return new Pair<>(null, exception);
             }
         }
 
         @Override
-        protected void onPostExecute(Proxmark3Device.TuneResult tuneResult) {
-            super.onPostExecute(tuneResult);
-
-            activity.updateTuneResult(tuneResult);
-
-            Toast.makeText(Proxmark3Activity.this, "Tune attempt completed", Toast.LENGTH_SHORT).show();
+        protected void onPostExecute(Pair<Proxmark3Device.TuneResult, IOException> result) {
+            super.onPostExecute(result);
 
             progressDialog.dismiss();
+
+            if (result == null)
+                return;
+
+            Proxmark3Activity proxmark3Activity = activity.get();
+            if (proxmark3Activity == null)
+                return;
+
+            Proxmark3Device.TuneResult tuneResult = result.first;
+            if (tuneResult == null) {
+                Toast.makeText(proxmark3Activity, "Failed to tune: " + result.second.getMessage(),
+                        Toast.LENGTH_LONG).show();
+                return;
+            }
+
+            Proxmark3TuneResultView tuneResultsView = new Proxmark3TuneResultView(proxmark3Activity);
+            tuneResultsView.setTuneResults(tuneResult);
+            tuneResultsView.setPadding(70, 30, 70, 30);
+
+            new AlertDialog.Builder(proxmark3Activity)
+                    .setTitle("Tune Results")
+                    .setView(tuneResultsView)
+                    .setPositiveButton(android.R.string.ok, new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+                            dialog.dismiss();
+                        }
+                    }).show();
         }
     }
 }
